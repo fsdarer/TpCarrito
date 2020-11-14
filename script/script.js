7df5906bcb5e1dab8b35750b785ba963dccae6b8
fsdarer <nicogorillaz274@gmail.com>
2020-11-14 10:45:46 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,57 @@
+const productos = [
+    {   
+        producto: "zanahorias",
+    },
+    {   
+        producto: "tomates",
+    },
+    {   
+        producto: "lechuga",
+    },
+    {   
+        producto: "apio",
+    },
+    {   
+        producto: "rucula",
+    },
+    {   
+        producto: "naranjas",
+    },
+    {   
+        producto: "manzanas",
+    },
+    {   
+        producto: "kiwi",
+    },
+    {   
+        producto: "sandia",
+    },
+    {   
+        producto: "melon",
+    },
+    {   
+        producto: "mandarina",
+    },
+    {   
+        producto: "limon",
+    },
+    {   
+        producto: "carbon",
+    },
+    {   
+        producto: "huevos",
+    },
+];
+
+    function crearTabla () {  
+        productos.forEach(producto => {
+
+            const tr = document.createElement("tr");
+
+            const tdProducto = document.createElement("td");
+            const tdProductoTxt = document.createTextNode(producto.producto);
+            tdProducto.appendChild(tdProductoTxt);
+        });
+    
+    }
+

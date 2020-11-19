072eb88b8607047e94da056a77e73c62fd439535
fsdarer <nicogorillaz274@gmail.com>
2020-11-18 22:13:25 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,73 +3,85 @@ const productos = [
     id: 1,
     producto: "Azucar Ledesma",
     precio: 50,
-    imagen: "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7792540260138_02.jpg",
+    imagen:
+      "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7792540260138_02.jpg",
   },
   {
     id: 2,
     producto: "Leche La Serenisima",
     precio: 78,
-    imagen: "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7793940448003_01_nuevopack.jpg",
+    imagen:
+      "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7793940448003_01_nuevopack.jpg",
   },
   {
     id: 3,
     producto: "Cafe Dolca",
     precio: 300,
-    imagen: "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/6/7613035070981_01_nuevopack.jpg"
+    imagen:
+      "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/6/7613035070981_01_nuevopack.jpg",
   },
   {
     id: 4,
     producto: "Alfajor Terrabusi",
     precio: 55,
-    imagen: "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/77939234_01.jpg"
+    imagen:
+      "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/77939234_01.jpg",
   },
   {
     id: 5,
     producto: "Mayonesa Natura",
     precio: 130,
-    imagen: "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7791866001364_02.jpg"
+    imagen:
+      "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7791866001364_02.jpg",
   },
   {
     id: 6,
     producto: "Jabon Skip",
     precio: 300,
-    imagen: "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7791290010055_05_nuevopack.jpg"
+    imagen:
+      "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7791290010055_05_nuevopack.jpg",
   },
   {
     id: 7,
     producto: "shampoo Elvive",
     precio: 180,
-    imagen: "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/5/7509552838954_01.jpg"
+    imagen:
+      "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/5/7509552838954_01.jpg",
   },
   {
     id: 8,
     producto: "Desodorante Dove",
     precio: 200,
-    imagen: "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/8/78924468_01_nuevopack.jpg"
+    imagen:
+      "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/8/78924468_01_nuevopack.jpg",
   },
   {
     id: 9,
     producto: "Pure de tomate La Campagnola",
     precio: 60,
-    imagen: "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7793360000485_01.jpg"
+    imagen:
+      "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7793360000485_01.jpg",
   },
   {
     id: 10,
     producto: "Servilletas Elite",
     precio: 80,
-    imagen: "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7790250023524_02.jpg"
+    imagen:
+      "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7790250023524_02.jpg",
   },
   {
     id: 11,
     producto: "Lavandina Ayudin",
     precio: 70,
-    imagen: "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7793253001360_01.jpg"
+    imagen:
+      "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7793253001360_01.jpg",
   },
   {
     id: 12,
     producto: "Aceite Natura",
     precio: 100,
-    imagen: "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7790272000046_02.jpg"
+    imagen:
+      "https://supermercado.carrefour.com.ar/media/catalog/product/cache/1/thumbnail/60x/9df78eab33525d08d6e5fb8d27136e95/7/7/7790272000046_02.jpg",
   },
 ];
 
